19a1d4e3e6b2b4b4f7f23107040b76d0682343ba
Kacper <luszczkacper42@gmail.com>
2023-05-20 21:10:47 +0200

    bug fixing

--- app/app.js
+++ app/app.js
@@ -2,6 +2,13 @@ const burgerButton = document.querySelector('.main-nav__burger-button');
 const mobileMenu = document.querySelector('.main-nav__links-container');
 const shadow = document.querySelector('.main-nav__mobile-shadow');
 const aboutPageHeaderText = document.querySelector('.main-header__text');
+const toggleButton = document.querySelectorAll(
+	'.directors__container-item-button'
+);
+const description = document.querySelectorAll(
+	'.directors__container-item-disabled'
+);
+
 const toggleMobileMenu = () => {
 	mobileMenu.classList.toggle('main-nav__links-container--active');
 	burgerButton.classList.toggle('main-nav__burger-button--active');
@@ -17,6 +24,23 @@ const toggleTextSize = () => {
 	}
 };
 
-console.log(burgerButton.children);
+const toggleDescription = (e) => {
+	// console.log(e.target);
+	let test = e.target.closest('div');
+	let test2 = test.children[1];
+	console.log(test);
+	// console.log(e.target.closest('div'));
+	// console.log(test.children[0]);
+	console.log();
+	// test2.classList.toggle('directors__container-item-enabled--active');
+	// e.target
+	// .closest('button')
+	// .classList.toggle('directors__container-item-button--active');
+};
 
+// console.log(burgerButton.children);
+
+toggleButton.forEach((button) =>
+	button.addEventListener('click', toggleDescription)
+);
 burgerButton.addEventListener('click', toggleMobileMenu);
